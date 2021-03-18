c9939dd64f2e368884438210d64031cab892e424
codemao <weiguangwei@codemao.cn>
2021-03-18 17:27:41 +0800

    初始化

---
+++ weiyada/src/main/java/org/weiyada/WeiYaDaApplication.java
@@ -0,0 +1,18 @@
+package org.weiyada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *@description:
+ *@return:
+ *@author: weiguangwei
+ *@time: 2021/3/18 5:05 下午
+ */
+@SpringBootApplication
+public class WeiYaDaApplication {
+    public static void main(String []args){
+        SpringApplication.run(WeiYaDaApplication.class);
+    }
+}
